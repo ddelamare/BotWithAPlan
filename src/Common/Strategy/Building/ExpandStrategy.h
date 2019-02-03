ad3ef6014554fb52df60b88d9e07fd4b4e16782b
ddelam <daniel.delamare@live.com>
2019-02-02 17:31:28 -0800

    Improved expansion placer

--- src/Common/Strategy/Building/ExpandStrategy.h
+++ src/Common/Strategy/Building/ExpandStrategy.h
@@ -21,18 +21,47 @@ public:
 	{
 		std::vector<QueryInterface::PlacementQuery> queries;
 		// All buildings are placed at the exactly center of a square 
-		auto roundedPoint = Point3D(round(startingPoint.x) + .5 , round(startingPoint.y) + .5 , 0);
+		auto roundedPoint = Point3D(round(startingPoint.x) + .5 , round(startingPoint.y) + .5 , 10);
 		if (direction.x > 0) direction.x = 1;
 		else if (direction.x < 0) direction.x = -1;
 		if (direction.y > 0) direction.y = 1;
 		else if (direction.y < 0) direction.y = -1;
 		bool foundSpot = false;
 		Point3D buildPos;
-		for (int i = 0; i < maxTries; i++)
+		bool isVertical = false;
+		bool isHorizontal = false;
+		if (direction.x == 0)
 		{
-			for (int j = 0; j < maxTries; j++)
+			isVertical = true;
+		}
+		if (direction.y == 0)
+		{
+			isHorizontal = true;
+		}
+
+		if (query->Placement(this->buildingAction, startingPoint))
+		{
+			return startingPoint;
+		}
+		else
+		{
+			debug->DebugSphereOut((startingPoint), 4, Colors::Teal);
+			debug->DebugTextOut(std::to_string(startingPoint.x) + " " + std::to_string(startingPoint.y), startingPoint);
+		}
+
+		for (int i = -maxTries; i < maxTries; i++)  
+		{
+			if (isHorizontal && (i * direction.x) < 0)
 			{
-				auto offset = Point3D((i * direction.x), (j * direction.y), 0);
+				continue;
+			}
+			for (int j = -maxTries; j < maxTries; j++)
+			{
+				if (isVertical && (j * direction.y) < 0)
+				{
+					continue;
+				}
+				auto offset = Point3D(i, j, 0);
 				debug->DebugSphereOut((roundedPoint + offset), 3, Colors::Yellow);
 				queries.push_back(QueryInterface::PlacementQuery(this->buildingAction, roundedPoint + offset));
 			}
@@ -46,7 +75,6 @@ public:
 			{
 				buildPos = Util::ToPoint3D(queries[i].target_pos);
 				foundSpot = true;
-				break;
 			}
 			else
 			{
@@ -66,7 +94,8 @@ public:
 		for (auto exp : state->ExpansionLocations)
 		{
 			// If enemy is too close don't expand there
-			if (Util::FindNearbyUnits(Unit::Alliance::Enemy, IsEnemy(), exp, obs, 10).size())
+			if (Util::FindNearbyUnits(Unit::Alliance::Enemy, IsEnemy(), exp, obs, 15).size() 
+				|| Util::FindNearbyUnits(Unit::Alliance::Self, IsTownHall(), exp, obs, 15).size())
 			{
 				continue;
 			}
@@ -90,7 +119,7 @@ public:
 			direction.z = 0;
 			Normalize3D(direction);
 			// Check for placement along the center vector
-			buildPos = GetFirstMatchingPlacement(exp, direction, query, debug, 3, NEXUS_MAX_TRIES);
+			buildPos = GetFirstMatchingPlacement(exp, direction, query, debug, 5, NEXUS_MAX_TRIES);
 
 			if (buildPos.x != 0 || buildPos.y != 0)
 				return buildPos;
